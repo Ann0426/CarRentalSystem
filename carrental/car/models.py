fc445796f1f9c7b2c60fd065094e533abbadd709
Ann <chiaan2844@gmail.com>
2020-11-26 13:48:33 -0500

    signup and loggin redierct to home page

--- carrental/car/models.py
+++ carrental/car/models.py
@@ -6,10 +6,10 @@ from django.urls import reverse
 
 ############## refer to others
 
-# from django.db import models
-# from django.contrib.auth.models import User
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # class Profile(models.Model):
 #     user = models.OneToOneField(User, on_delete=models.CASCADE)
--- carrental/car/views.py
+++ carrental/car/views.py
@@ -45,6 +45,11 @@ def signUp(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.profile.birth_date = form.cleaned_data.get('birth_date')
+            user.save()
+           
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)

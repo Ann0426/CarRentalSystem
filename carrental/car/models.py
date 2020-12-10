93abca95ae7004678445c7c9dd729e1c228688d6
Akshay Rajeev <akshayrajeev@github.com>
2020-12-10 17:13:08 -0500

    changes-signup form

--- carrental/car/models.py
+++ carrental/car/models.py
@@ -21,44 +21,7 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     bio = models.TextField(max_length=500, blank=True)
-#     location = models.CharField(max_length=30, blank=True)
-#     birth_date = models.DateField(null=True, blank=True)
 
-# @receiver(post_save, sender=User)
-# def update_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-#     instance.profile.save()
-
-
-
-# class Car(models.Model):
-#     name = models.CharField(max_length=100)
-#     description = models.TextField()
-#     daily_rent = models.IntegerField()
-
-#     is_available = models.BooleanField()
-#     objects = models.Manager()
-
-#     def get_absolute_url(self):
-#         return reverse('car-details', kwargs={'pk': self.pk})
-
-#     def __str__(self):
-#         return self.name
-    
-# class Booking(models.Model):
-#     car = models.ForeignKey(Car, on_delete=models.CASCADE)
-
-#     customer_name = models.CharField(max_length=100)
-#     customer_email = models.EmailField()
-#     customer_phone_number = models.TextField()
-
-#     booking_start_date = models.DateField()
-#     booking_end_date = models.DateField()
-#     booking_message = models.TextField()
-
-#     is_approved = models.BooleanField()
-#     objects = models.Manager()
+class Customer(models.Model):
+    username = models.CharField(max_length=255)
+    custid = models.IntegerField()
--- carrental/car/views.py
+++ carrental/car/views.py
@@ -3,31 +3,34 @@ from django.http import HttpResponse
 from django.http import Http404
 import datetime
 # from django.contrib.auth.models import User
-from .signinform import SignUpForm
-import datetime
-
+from .forms import SignUpForm
+from datetime import datetime, timedelta
+from django.contrib.auth.decorators import login_required
+from .utils import *
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from .utils import get_office_locations
 from django.apps import apps
-from .utils import get_dates
-from .utils import get_available_cars, get_car_info, get_car_class_info,get_coupon_info,get_location_info,calculate_total
+
+from .models import Customer
+
 # from django.views.generic import DetailView
 from django.views.generic import TemplateView, ListView
-# from car.models import Booking
-# from car.models import Car
-# from django.db.models import Q 
+
 from django.contrib.auth.models import User
+import json
 
 
 connection = apps.get_app_config('car').connection
 
-start_date = ""
-end_date = ""
-car = []
-location = []
-location2 = []
+start_date = datetime.now()
+end_date = datetime.now() + timedelta(days=1)
+location = ''
+location2 = ''
+car_rent = []
+car_info = []
+
+
 def home(request):
     if request.method == 'GET':
         location_list = get_office_locations(connection)
@@ -48,6 +51,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
+            print(form.cleaned_data)
             return redirect('/')
     else:
         form = SignUpForm()
@@ -55,105 +59,48 @@ def signup(request):
 
 
 def search(request):
-    global start_date,end_date,location,location2
+    global start_date, end_date
+    global location, location2
     location = request.GET['location']
     location2 = request.GET['location2']
-    start_date = request.GET['Start']
+    start_date = request.GET['start']
     end_date = request.GET['end']
     cars = get_available_cars(connection, location, 'all')
-    location = get_location_info(connection,location)
-    location2 = get_location_info(connection,location2)
+    location = get_location_info(connection, location)
+    location2 = get_location_info(connection, location2)
     print(cars)
     
-    start_date = start_date.split(' ', 1 )[0] 
+    start_date = start_date.split(' ', 1 )[0]
     end_date = end_date.split(' ', 1 )[0]
-    print(start_date)
-    print(end_date)
-    print(type(start_date))
+    #print(start_date)
+    #print(end_date)
 
     return render(request, 'car/search.html', {"cars": cars})
 
+
+@login_required(login_url='/login/')
 def booking(request):
-        global car,car_info,car_rent 
-        car = request.GET['BOOK']
-        if request.user.is_authenticated:
-            car_info = get_car_info(connection,car)
-        if 'type_id' in car_info[0]:
-            car_rent = get_car_class_info(connection,car_info[0]['type_id'])
-        current_user = request.user
-        print(car_info)
-        print(type(car_rent[0]['rent_charge']))
+    global car_rent, car_info
+    car = request.GET['BOOK']
+    if request.user.is_authenticated:
+        car_info = get_car_info(connection, car)
+
+    if 'type_id' in car_info[0]:
+        car_rent = get_car_class_info(connection, car_info[0]['type_id'])
+    current_user = request.user
+    print(car_info)
+    print(type(car_rent[0]['rent_charge']))
         
-        return render(request, 'car/new_booking.html', {"car": car_info,"user":current_user,"dates": get_dates(),"rent":car_rent , "start_date":start_date,"end_date":end_date,"location":location,"location2":location2}  )
+    return render(request, 'car/new_booking.html', {"car": car_info, "user":current_user,"dates": get_dates(),"rent":car_rent , "start_date":start_date,"end_date":end_date,"location":location,"location2":location2}  )
         
 
 def invoices(request):
+
     coupon = request.GET['coupon']
-    coupon_amount = get_coupon_info(connection,coupon)
+    coupon_amount = get_coupon_info(connection, coupon)
     print(coupon_amount)
     current_user = request.user
-    total_amount = calculate_total(start_date,end_date,coupon_amount[0]['discount'],car_rent[0]['rent_charge'])
+    total_amount = calculate_total(start_date, end_date,coupon_amount[0]['discount'],car_rent[0]['rent_charge'])
     return render(request, 'car/invoices.html', {"car": car_info,"user":current_user,"dates": get_dates(),"rent":car_rent ,"coupon":coupon_amount,"start_date":start_date,"end_date":end_date,"total_amount":total_amount,})
-    
- 
 
 
-############## refer to others
-# class HomeView(TemplateView):
-#     template_name = 'car/homes.html'
-
-#     def get_context_data(self, **kwargs):
-#         ctx = super(HomeView, self).get_context_data(**kwargs)
-
-#         cars = Car.objects.filter(is_available=True)
-#         ctx['cars'] = cars
-
-#         return ctx
-
-
-# class CarDetailsView(DetailView):
-#     template_name = 'car/car_details.html'
-#     model = Car
-
-#     def get_context_data(self, **kwargs):
-#         ctx = super(CarDetailsView, self).get_context_data(**kwargs)
-#         ctx['booking_success'] = 'booking-success' in self.request.GET
-
-#         return ctx
-
-
-# class NewBookingView(CreateView):
-#     model = Booking
-#     fields = [
-#         'customer_name', 'customer_email', 'customer_phone_number',
-#         'booking_start_date', 'booking_end_date', 'booking_message'
-#     ]
-
-#     template_name = 'car/new_booking.html'
-
-#     def get_car(self):
-#         car_pk = self.kwargs['car_pk']
-#         car = Car.objects.get(pk=car_pk)
-
-#         return car
-
-#     def get_context_data(self, **kwargs):
-#         ctx = super(NewBookingView, self).get_context_data(**kwargs)
-#         ctx['car'] = self.get_car()
-
-#         return ctx
-
-#     def form_valid(self, form):
-#         new_booking = form.save(commit=False)
-#         new_booking.car = self.get_car()
-#         new_booking.is_approved = False
-
-#         new_booking.save()
-
-#         return super(NewBookingView, self).form_valid(form)
-
-#     def get_success_url(self):
-#         car = self.get_car()
-#         car_details_page_url = car.get_absolute_url()
-
-#         return '{}?booking-success=1'.format(car_details_page_url)
